f284f9cbb40b8c0c998486f6661a4b22f0c6714c
wegatron <wegatron@gmail.com>
2020-05-08 15:25:25 +0800

    have a bug

--- point_cloud_course/HW4/ground_seg.cpp
+++ point_cloud_course/HW4/ground_seg.cpp
@@ -1,6 +1,7 @@
 #include "ground_seg.h"
 #include <math.h>
 #include <iomanip>
+#include <chrono>
 #include <zsw_vtk_io.h>
 
 
@@ -63,9 +64,8 @@ void gaussian_process_ground_seg::extract_seed(const int ind,
         } else sig_pts.emplace_back(pb[j]);
     }
 
-    std::cout << "rh pts:" << rh_pts_[ind].size() << std::endl;
-
-    #if 1
+    //std::cout << "rh pts:" << rh_pts_[ind].size() << std::endl;
+    #if 0
     {
         std::stringstream ss;
         ss << "/home/wegatron/tmp/seed" << std::setw(4) << std::setfill('0') << ind << ".vtk";
@@ -87,6 +87,7 @@ void gaussian_process_ground_seg::extract_seed(const int ind,
 
 }
 
+
 std::vector<uint8_t> gaussian_process_ground_seg::segment(pcl::PointCloud<pcl::PointXYZ>::Ptr &pts)
 {
     const auto num_pts = pts->size();
@@ -96,22 +97,50 @@ std::vector<uint8_t> gaussian_process_ground_seg::segment(pcl::PointCloud<pcl::P
     std::vector<range_height> current_model;
     std::vector<range_height> sig_pts;
     for(int i=0; i<params_.num_bins_a; ++i) {
+        std::cout << i << "/" << params_.num_bins_a << std::endl;
         extract_seed(i, pts, current_model, sig_pts);
         if(current_model.size() <= 2) continue;
+        std::cout << "insac..." << std::endl;
         // insac
-    }
+        insac(pts, 100, current_model, sig_pts);
 
+        break;
+        // result to labels
+        for(const auto &tmp_rh : current_model)
+            labels[tmp_rh.index] = 1;
+    }
+    #if 1
+    std::tuple<std::string, int, std::vector<float>> attr;
+    std::get<0>(attr) = "is_ground";
+    std::get<1>(attr) = 1; // attribute dimension
+    auto &values = std::get<2>(attr);
+    values.resize(labels.size());
+    for(int i=0; i<labels.size(); ++i)
+        values[i] = labels[i];
+    zsw::point_cloud2vtk_file2("/home/wegatron/segment.vtk", pts, {attr});
+    #endif
     return labels;
-#if 0
+}
+
+void gaussian_process_ground_seg::insac(const pcl::PointCloud<pcl::PointXYZ>::Ptr &pts,
+                                        const int max_iter,
+                                        std::vector<range_height> &current_model,
+                                        std::vector<range_height> &sig_pts)
+{
     // insac
-    const float len_scale = 1.0/(params_.p_l * params_.p_l);
-    while(true)
-    {
+    const float len_scale = 1.0/(2*params_.p_l*params_.p_l);
+    int alter_cnt = 0;
+    int itr_cnt = 0;
+    int tc_pre = 0;
+    int tc_val = 0;
+    int tc_evaluate = 0;
+    do {
+        auto tp0 = std::chrono::high_resolution_clock::now();
         // calculate K_ff^{-1}, K_fy, K_yy
         const int model_size = current_model.size();
         const int sig_size = sig_pts.size();
+
         Eigen::VectorXf train_x(model_size);
-        Eigen::VectorXf x(sig_size);
         Eigen::VectorXf train_y(model_size);
         for(int j=0; j<model_size; ++j)
         {
@@ -119,10 +148,9 @@ std::vector<uint8_t> gaussian_process_ground_seg::segment(pcl::PointCloud<pcl::P
             train_y[j] = current_model[j].height;
         }
 
+        Eigen::VectorXf x(sig_size);
         for(int j=0; j<sig_size; ++j)
-        {
             x[j] = sig_pts[j].r;
-        }
 
         Eigen::VectorXf sqr_train_x = train_x.array() * train_x.array();
         Eigen::VectorXf sqr_x = x.array() * x.array();
@@ -137,11 +165,13 @@ std::vector<uint8_t> gaussian_process_ground_seg::segment(pcl::PointCloud<pcl::P
                 k_1.col(j).setConstant(sqr_train_x[j]);
             }
 
-            K_ff = -len_scale * (k_0 + k_1 - 2*train_x * train_x.transpose());
+            K_ff = -len_scale * (k_0 + k_1 - 2*train_x*train_x.transpose());
             K_ff = params_.p_sf * exp(K_ff.array()).matrix()
                 + params_.p_sn * Eigen::MatrixXf::Identity(model_size, model_size);
             inv_K_ff = K_ff.inverse();
         }
+
+        // std::cout << "inv_K_ff:\n" << inv_K_ff << std::endl;
 
         Eigen::MatrixXf K_yy(sig_size, sig_size);
         {
@@ -151,9 +181,11 @@ std::vector<uint8_t> gaussian_process_ground_seg::segment(pcl::PointCloud<pcl::P
                 k_0.row(j).setConstant(sqr_x[j]);
                 k_1.col(j).setConstant(sqr_x[j]);
             }
-            K_yy = -len_scale*(k_0 + k_1 - 2*sqr_x * sqr_x.transpose());
+            K_yy = -len_scale*(k_0 + k_1 - 2*x*x.transpose());
             K_yy = params_.p_sf * exp(K_yy.array());
         }
+
+        // std::cout << "K_yy:\n" << K_yy << std::endl;
 
         Eigen::MatrixXf K_fy(model_size, sig_size);
         {
@@ -165,16 +197,72 @@ std::vector<uint8_t> gaussian_process_ground_seg::segment(pcl::PointCloud<pcl::P
             for(int j=0; j<sig_size; ++j)
                 k_1.col(j).setConstant(sqr_x[j]);
 
-            K_fy = -len_scale*(k_0 + k_1 - 2*sqr_train_x * sqr_x.transpose());
+            K_fy = -len_scale*(k_0 + k_1 - 2*train_x*x.transpose());
             K_fy = params_.p_sf * exp(K_fy.array());
         }
 
+        auto tp1 = std::chrono::high_resolution_clock::now();
+        tc_pre += std::chrono::duration_cast<std::chrono::milliseconds>(tp1 - tp0).count();
+
+        // std::cout << "K_fy:\n" << K_fy << std::endl;
+
         // calc y and cov
-        Eigen::VectorXf y = K_fy.transpose() * inv_K_ff * train_y;
-        Eigen::MatrixXf cov =K_yy - K_fy.transpose() * inv_K_ff * K_fy;
+        Eigen::MatrixXf tmp_calc = K_fy.transpose() * inv_K_ff;
+        Eigen::VectorXf y = tmp_calc * train_y;
+        Eigen::MatrixXf cov = K_yy - tmp_calc * K_fy;
+
+        auto tp2 = std::chrono::high_resolution_clock::now();
+        tc_val = std::chrono::duration_cast<std::chrono::milliseconds>(tp2-tp1).count();
 
         // evaluate the model
-    }
-    return labels;
+        alter_cnt = 0;
+        for(int i=0; i<sig_size; ++i)
+        {
+             std::cout << sig_pts[i].height << " " << y[i] << " " << cov(i,i) << std::endl;
+             std::cout << "diff=" << fabs(sig_pts[i].height - y[i])
+                       << "th="
+                       << params_.p_tdata/(sqrt(params_.p_sn + cov(i, i)))
+                       << std::endl;
+            if(y[i] < params_.p_tmodel // y[i] seems fine
+               && fabs(sig_pts[i].height - y[i]) // uncertainty not very large
+               < params_.p_tdata/(sqrt(params_.p_sn + cov(i, i))))
+            {
+                current_model.push_back(sig_pts[i]);
+                sig_pts[i] = sig_pts[sig_pts.size()-1];
+                sig_pts.pop_back();
+                ++alter_cnt;
+            }
+        }
+
+        auto tp3 = std::chrono::high_resolution_clock::now();
+        tc_evaluate = std::chrono::duration_cast<std::chrono::milliseconds>(tp3-tp2).count();
+        // std::cout << "y:" << y.transpose() << std::endl;
+        // std::cout << "cov:\n" << cov << std::endl;
+        std::cout << "alter_cnt:" << alter_cnt << std::endl;
+        std::cout << "model_pts_num=" << current_model.size()
+                  << " sig_pts_num=" << sig_pts.size() << std::endl;
+        // debug output
+#if 1
+        {
+            std::stringstream ss;
+            ss << "/home/wegatron/tmp/insac_"
+               << std::setw(4) << std::setfill('0') << itr_cnt << ".vtk";
+            pcl::PointCloud<pcl::PointXYZ>::Ptr model_pc(new pcl::PointCloud<pcl::PointXYZ>);
+            model_pc->resize(current_model.size());
+            for(int i=0; i<current_model.size(); ++i)
+            {
+                model_pc->points[i] = pts->points[current_model[i].index];
+            }
+            pcl::PointCloud<pcl::PointXYZ>::Ptr sig_pc(new pcl::PointCloud<pcl::PointXYZ>);
+            sig_pc->resize(sig_pts.size());
+            for(int i=0; i<sig_pts.size(); ++i)
+            {
+                sig_pc->points[i] = pts->points[sig_pts[i].index];
+            }
+            zsw::point_clouds2vtk_file(ss.str(), {model_pc, sig_pc});
+        }
 #endif
+    }while(alter_cnt >0 && ++itr_cnt < max_iter);
+    std::cout << "tc_pre=" << tc_pre << " tc_val=" << tc_val << " tc_evaluate=" << tc_evaluate << std::endl;
+    std::cout << "itr " << itr_cnt << std::endl;
 }
--- point_cloud_course/HW4/ground_seg.h
+++ point_cloud_course/HW4/ground_seg.h
@@ -13,18 +13,13 @@ struct ground_seg_params {
     size_t num_seed_points = 10;
 
     // for GP model
-    float p_l = 4;  // length parameter, how close points have to be in the
+    float p_l = 2;  // length parameter, how close points have to be in the
     // GP model to correlate them
-    float p_sf = 1;    // scaling on the whole covariance function
-    float p_sn = 0.3;  // the expected noise for the mode
+    float p_sf = 2;    // scaling on the whole covariance function
+    float p_sn = 0.4;  // the expected noise for the mode
     float p_tmodel = 5;  // the required confidence required in order to consider
     // something ground
-    float p_tdata = 5;  // scaled value that is required for a query point to be
-    // considered ground
-    float p_tg = 0.3;  // ground height threshold
-
-    /// Height the robot (m), used to distinguish "drivable" overhanging points
-    float robot_height = 1.2;
+    float p_tdata = 0.5;  // scaled value that is required for a query point to be
 
     // seeding parameters
     float max_seed_range = 50;   // meters
@@ -45,21 +40,24 @@ struct range_height
 class gaussian_process_ground_seg
 {
 public:
-  gaussian_process_ground_seg(const ground_seg_params &params)
-    : params_(params), rh_pts_(params.num_bins_a) {}
+    gaussian_process_ground_seg(const ground_seg_params &params)
+        : params_(params), rh_pts_(params.num_bins_a) {}
 
-  /**
-   * \brief segment point cloud, return labels, 1 for ground and 0 for nonground
-   */
-  std::vector<uint8_t> segment(pcl::PointCloud<pcl::PointXYZ>::Ptr &pts);
+    /**
+     * \brief segment point cloud, return labels, 1 for ground and 0 for nonground
+     */
+    std::vector<uint8_t> segment(pcl::PointCloud<pcl::PointXYZ>::Ptr &pts);
 private:
-  void gen_polar_bin_grid(pcl::PointCloud<pcl::PointXYZ>::Ptr &pts);
-  void extract_seed(const int ind,
-                    const pcl::PointCloud<pcl::PointXYZ>::Ptr &pts,
-                    std::vector<range_height> &current_model,
-                    std::vector<range_height> &sig_pts);
-  Eigen::MatrixXd gp_kernel(const Eigen::VectorXd &x0, const Eigen::VectorXd &x1);
-  ground_seg_params params_;
-  std::vector<std::vector<range_height>> rh_pts_;
+    void gen_polar_bin_grid(pcl::PointCloud<pcl::PointXYZ>::Ptr &pts);
+    void extract_seed(const int ind,
+                      const pcl::PointCloud<pcl::PointXYZ>::Ptr &pts,
+                      std::vector<range_height> &current_model,
+                      std::vector<range_height> &sig_pts);
+    Eigen::MatrixXd gp_kernel(const Eigen::VectorXd &x0, const Eigen::VectorXd &x1);
+    void insac(const pcl::PointCloud<pcl::PointXYZ>::Ptr &pts,
+               const int max_iter, std::vector<range_height> &model,
+               std::vector<range_height> &sig_pts);
+    ground_seg_params params_;
+    std::vector<std::vector<range_height>> rh_pts_;
 };
 #endif //GROUND_SEG_H
